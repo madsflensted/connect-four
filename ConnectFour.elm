284ccdd573f0395182821e1cd7fa6639ffec5f01
Mads Flensted-Urech <mfu@coadu.com>
2015-11-13 23:39:12 +0100

    Working game

--- ConnectFour.elm
+++ ConnectFour.elm
@@ -4,61 +4,98 @@ import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import List exposing (..)
+import Mouse
+import Signal
 
 -- Model
+type Turn = PlayerA | PlayerB
+
 type Slot = Empty | A | B
 
 type alias Column = List Slot
 
 type alias Board = List Column
 
+type alias Model =
+  { board: Board
+  , turn : Turn
+  }
+
 maxColumn = 7
 maxRow = 6
+pieceSize = 90 -- px
 
-emptyBoard = 
+emptyBoard =
   repeat maxColumn (repeat maxRow Empty)
+
+startModel =
+  { board = emptyBoard
+  , turn = PlayerA
+  }
 
 -- View
 
-view : Board -> Element
-view board =
-  flow right <| map viewColumn board
-  
+view : Model -> Element
+view model =
+  flow right <| map viewColumn model.board
+
 viewColumn column =
   flow down <| map viewSlot column
 
-viewSlot slot =  
-  let pieceSize = 90
-      pieceRadius = (pieceSize - 15) / 2
-      pieceColor piece = 
-        case slot of 
+viewSlot slot =
+  let pieceRadius = (pieceSize - 15) / 2
+      pieceColor piece =
+        case slot of
            Empty -> white
            A -> blue
            B -> red
-  in collage pieceSize pieceSize 
+  in collage pieceSize pieceSize
     [ circle pieceRadius |> filled (pieceColor slot)]
     |> color orange
-    
-    
+
+
 -- Update
-update : (Slot, Int) -> Board -> Board
-update move board =
-  List.indexedMap (updateColumn move) board
-  
+update : Int -> Model -> Model
+update column model =
+  { model |
+        board <- List.indexedMap (updateColumn (model.turn, column)) model.board
+      , turn <- nextPlayer model.turn
+  }
+
 updateColumn (player, inColumn) index column =
   if inColumn /= index then
     column
   else
     player `dropIn` column
-    
+
 dropIn player column =
     let (empty, nonEmpty) = List.partition ((==) Empty) column
-        filled = player :: nonEmpty
+        filled = (playerToSlot player) :: nonEmpty
         empties = repeat (maxRow - (length filled)) Empty
     in empties `append` filled
 
-    
--- Main
-moves = [(A, 3), (B, 3), (A, 0), (B, 2), (A, 2), (B, 3)]
+playerToSlot player =
+  case player of
+    PlayerA -> A
+    PlayerB -> B
 
-main = view <| foldl update emptyBoard moves
+nextPlayer : Turn -> Turn
+nextPlayer turn =
+  case turn of
+    PlayerA -> PlayerB
+    PlayerB -> PlayerA
+
+-- Signals
+
+toColumnIndex : Int -> Int
+toColumnIndex x =
+  (x // pieceSize) % maxColumn
+
+dropPieceAt : Signal Int
+dropPieceAt =
+  Signal.sampleOn Mouse.clicks Mouse.x
+
+-- Main
+testMoves = [(A, 3), (B, 3), (A, 0), (B, 2), (A, 2), (B, 3)]
+
+main = Signal.map view <| Signal.foldp update startModel <| Signal.map toColumnIndex dropPieceAt
